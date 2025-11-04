5debf1c682506dfb018af6dca516aebf97046a33
sdi2500111 <stratoslamprinos07@gmail.com>
2025-11-04 14:36:00 +0200

    aaa

--- calc.c
+++ calc.c
@@ -2,8 +2,45 @@
 
 int main()
 {
-    int num1;
-    int num2;
+    long long num1;
+    long long num2;
+    char sym;
+
+    scanf(" %lld", &num1);
+    scanf(" %c", &sym);
+    scanf(" %lld", &num2);
+
+    switch(sym)
+    {
+        case '+':
+            printf(" %lld", num1 + num2);
+            break;
+
+        case '-':
+            printf(" %lld", num1 - num2);
+            break;
+
+        case '*':
+            printf(" %lld", num1 * num2);
+            break;
+
+        case '/':
+            if (num2 != 0)
+            {
+                printf(" %lld", num1 / num2);
+
+            }
+            else
+            {
+                printf("Error");
+
+            }
+            break;
+        default:
+            printf("Error");
+            
+    }
+    
 
 
     return 0;
